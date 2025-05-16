d013bc8ff821a59d59cde4bda554b6de0b8241b7
Krisiiii98 <43846045+Krisiiii98@users.noreply.github.com>
2025-05-16 20:08:55 +0200

    Delete screen_size.py

--- screen_size.py
+++
@@ -1,9 +0,0 @@
-import ctypes
-
-ctypes.windll.shcore.SetProcessDpiAwareness(2)
-ctypes.windll.shcore.SetProcessDpiAwareness(1)
-user32 = ctypes.windll.user32
-screensize1 = user32.GetSystemMetrics(78)
-screensize2 = user32.GetSystemMetrics(79)
-
-print(screensize1, screensize2)
